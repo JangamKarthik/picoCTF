ff65ca97f343bf8da57341a995d3755468de8214
Karthik_Jangam <102133551+JangamKarthik@users.noreply.github.com>
2023-01-08 12:31:49 +0530

    Create gsk-convertme.py

---
+++ gsk-convertme.py
@@ -0,0 +1,9 @@
+to start of copy the link address of the python script file
+then go to webshell use wget command to download the file
+type- wget and paste the address
+now for running the .py files on your webshell use the command python3 'filename'
+type- python3 convertme.py
+now answer the question i was asked 17 in binary
+i used this site for converting this-https://www.rapidtables.com/convert/number/decimal-to-binary.html
+now submit the answer 
+boom you got your flag
